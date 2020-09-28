368f03befe98952799dfbec2ff83a2225a4e8dd6
Artem Meges <artemmeges@gmail.com>
2020-09-28 23:47:55 +0300

    add header adaptive

--- index.js
+++ index.js
@@ -7,27 +7,29 @@ window.addEventListener('scroll', () => {
     const scrolledSections = [...sections].filter(section => {
         const coords = section.getBoundingClientRect();
 
-        if (coords.top < windowHeight / 2) return true
+        if (coords.top < windowHeight / 2.1) return true
         else return false;
     });
 
     const lastScrolled = scrolledSections[scrolledSections.length - 1];
 
+    if (!lastScrolled) return;
+    
     switch(lastScrolled.id) {
         case 'general':
-            navHighlight.style.top = '14px';
+            navHighlight.style.top = '5%';
             break;
         case 'skills':
-            navHighlight.style.top = '69px';
+            navHighlight.style.top = '27%';
             break;
         case 'projects':
-            navHighlight.style.top = '123px';
+            navHighlight.style.top = '101px';
             break;
         case 'certification':
-            navHighlight.style.top = '176px';
+            navHighlight.style.top = '71%';
             break;
         case 'education':
-            navHighlight.style.top = '230px';
+            navHighlight.style.top = '93%';
             break;            
     } 
 });
