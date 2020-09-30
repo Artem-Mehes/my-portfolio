1b6dee48dc96c4492e93c0a188791070308891d2
Artem Meges <artemmeges@gmail.com>
2020-09-30 23:13:57 +0300

    about adaptive

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const navHighlight = document.querySelector('#nav-highlight'),
     sections = document.querySelectorAll('.section');
 
+
 const windowHeight = document.documentElement.clientHeight;
 
-window.addEventListener('scroll', () => {
-    const scrolledSections = [...sections].filter(section => {
-        const coords = section.getBoundingClientRect();
-
-        if (coords.top < windowHeight / 2.1) return true
-        else return false;
-    });
-
-    const lastScrolled = scrolledSections[scrolledSections.length - 1];
-
-    if (!lastScrolled) return;
+if (navHighlight.style.display != 'none') {
+    window.addEventListener('scroll', () => {
+        const scrolledSections = [...sections].filter(section => {
+            const coords = section.getBoundingClientRect();
     
-    switch(lastScrolled.id) {
-        case 'general':
-            navHighlight.style.top = '5%';
-            break;
-        case 'skills':
-            navHighlight.style.top = '27%';
-            break;
-        case 'projects':
-            navHighlight.style.top = '101px';
-            break;
-        case 'certification':
-            navHighlight.style.top = '71%';
-            break;
-        case 'education':
-            navHighlight.style.top = '93%';
-            break;            
-    } 
-});
+            if (coords.top < windowHeight / 2.1) return true
+            else return false;
+        });
+        const lastScrolled = scrolledSections[scrolledSections.length - 1];
+        if (!lastScrolled) return;
+        
+        switch(lastScrolled.id) {
+            case 'general':
+                navHighlight.style.top = '5%';
+                break;
+            case 'skills':
+                navHighlight.style.top = '27%';
+                break;
+            case 'projects':
+                navHighlight.style.top = '49%';
+                break;
+            case 'certification':
+                navHighlight.style.top = '71%';
+                break;
+            case 'education':
+                navHighlight.style.top = '93%';
+                break;            
+        } 
+    });
+}
