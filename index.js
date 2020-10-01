adb14b6fdba4fc20b158a95141087c80903e9b09
Artem Meges <artemmeges@gmail.com>
2020-10-01 11:47:35 +0300

    add resize screen event

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 const navHighlight = document.querySelector('#nav-highlight'),
     sections = document.querySelectorAll('.section');
 
-
 const windowHeight = document.documentElement.clientHeight;
 
-if (navHighlight.style.display != 'none') {
-    window.addEventListener('scroll', () => {
-        const scrolledSections = [...sections].filter(section => {
-            const coords = section.getBoundingClientRect();
-    
-            if (coords.top < windowHeight / 2.1) return true
-            else return false;
-        });
-        const lastScrolled = scrolledSections[scrolledSections.length - 1];
-        if (!lastScrolled) return;
-        
-        switch(lastScrolled.id) {
-            case 'general':
-                navHighlight.style.top = '5%';
-                break;
-            case 'skills':
-                navHighlight.style.top = '27%';
-                break;
-            case 'projects':
-                navHighlight.style.top = '49%';
-                break;
-            case 'certification':
-                navHighlight.style.top = '71%';
-                break;
-            case 'education':
-                navHighlight.style.top = '93%';
-                break;            
-        } 
+const moveNavHighlight = () => {
+    const scrolledSections = [...sections].filter(section => {
+        const coords = section.getBoundingClientRect();
+
+        if (coords.top < windowHeight / 2.1) return true
+        else return false;
     });
+    const lastScrolled = scrolledSections[scrolledSections.length - 1];
+    if (!lastScrolled) return;
+    
+    switch(lastScrolled.id) {
+        case 'general':
+            navHighlight.style.top = '5%';
+            break;
+        case 'skills':
+            navHighlight.style.top = '27%';
+            break;
+        case 'projects':
+            navHighlight.style.top = '49%';
+            break;
+        case 'certification':
+            navHighlight.style.top = '71%';
+            break;
+        case 'education':
+            navHighlight.style.top = '93%';
+            break;            
+    } 
 }
+
+const checkScreenWidth = () => {
+    if (!window.matchMedia('(max-width: 1080px)').matches) {
+        window.addEventListener('scroll', moveNavHighlight);
+    } else {
+        window.removeEventListener('scroll', moveNavHighlight);
+    }
+}
+
+checkScreenWidth();
+
+window.addEventListener('resize', checkScreenWidth);
