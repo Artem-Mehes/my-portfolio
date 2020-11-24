ee7b69cb92c532835a1279774c0dd26c6844fa3d
Artem Meges <artemmeges@gmail.com>
2020-11-24 21:58:46 +0200

    add theme toggle

--- index.js
+++ index.js
@@ -1,52 +1,67 @@
 const navHighlight = document.querySelector('#nav-highlight'),
-    sections = document.querySelectorAll('.section'),
-    burgerWrapper = document.querySelector('#burger-wrapper'),
-    burger = document.querySelector('#burger'),
-    nav = document.querySelector('#nav');
+	sections = document.querySelectorAll('.section'),
+	burgerWrapper = document.querySelector('#burger-wrapper'),
+	burger = document.querySelector('#burger'),
+	nav = document.querySelector('#nav'),
+	toggleTheme = document.querySelector('#toggle-theme');
 
 const windowHeight = document.documentElement.clientHeight;
 
-const moveNavHighlight = () => {
-    const scrolledSections = [...sections].filter(section => {
-        const coords = section.getBoundingClientRect();
+const setPreferedColorTheme = () => {
+	if (
+		window.matchMedia &&
+		window.matchMedia('(prefers-color-scheme: dark)').matches
+	) {
+		document.documentElement.classList.add('dark-theme');
+	}
+};
 
-        if (coords.top < windowHeight / 2.1) return true
-        else return false;
-    });
-    const lastScrolled = scrolledSections[scrolledSections.length - 1];
-    if (!lastScrolled) return;
-    
-    switch(lastScrolled.id) {
-        case 'general':
-            navHighlight.style.top = '5%';
-            break;
-        case 'skills':
-            navHighlight.style.top = '27%';
-            break;
-        case 'projects':
-            navHighlight.style.top = '49%';
-            break;
-        case 'certification':
-            navHighlight.style.top = '71%';
-            break;
-        case 'education':
-            navHighlight.style.top = '93%';
-            break;            
-    } 
-}
+const moveNavHighlight = () => {
+	const scrolledSections = [...sections].filter((section) => {
+		const coords = section.getBoundingClientRect();
+
+		if (coords.top < windowHeight / 2.1) return true;
+		else return false;
+	});
+	const lastScrolled = scrolledSections[scrolledSections.length - 1];
+	if (!lastScrolled) return;
+
+	switch (lastScrolled.id) {
+		case 'general':
+			navHighlight.style.top = '5%';
+			break;
+		case 'skills':
+			navHighlight.style.top = '27%';
+			break;
+		case 'projects':
+			navHighlight.style.top = '49%';
+			break;
+		case 'certification':
+			navHighlight.style.top = '71%';
+			break;
+		case 'education':
+			navHighlight.style.top = '93%';
+			break;
+	}
+};
 
 const checkScreenWidth = () => {
-    if (!window.matchMedia('(max-width: 1080px)').matches) {
-        window.addEventListener('scroll', moveNavHighlight);
-    } else {
-        window.removeEventListener('scroll', moveNavHighlight);
-    }
-}
+	if (!window.matchMedia('(max-width: 1080px)').matches) {
+		window.addEventListener('scroll', moveNavHighlight);
+	} else {
+		window.removeEventListener('scroll', moveNavHighlight);
+	}
+};
 
 window.addEventListener('resize', checkScreenWidth);
 burgerWrapper.addEventListener('click', () => {
-    burger.classList.toggle('open');
-    nav.classList.toggle('open');
+	burger.classList.toggle('open');
+	nav.classList.toggle('open');
 });
 
+toggleTheme.addEventListener('click', () => {
+	document.documentElement.classList.toggle('dark-theme');
+});
+
+setPreferedColorTheme();
 checkScreenWidth();
